0bca96f0617fe5931f61b66f270c843eabe71875
waynergt <wlopezy2@miumg.edu.gt>
2025-08-08 23:31:29 -0600

    modificando el footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,13 +4,13 @@ const Footer: React.FC = () => (
   <footer className="bg-dark text-white py-4 mt-5">
     <div className="container text-center">
       <p>Síguenos en:</p>
-      <a href="https://twitter.com/" className="text-white me-3">
+      <a href="https://twitter.com/_wayner" className="text-white me-3">
         <i className="bi bi-twitter"></i> Twitter
       </a>
-      <a href="https://facebook.com/" className="text-white me-3">
+      <a href="https://facebook.com/albertolopezgt" className="text-white me-3">
         <i className="bi bi-facebook"></i> Facebook
       </a>
-      <a href="https://instagram.com/" className="text-white">
+      <a href="https://instagram.com/_wayner" className="text-white">
         <i className="bi bi-instagram"></i> Instagram
       </a>
     </div>
