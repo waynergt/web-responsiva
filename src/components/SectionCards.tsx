ff3817219b08b37f38058cd1d28fe56c87fe6ba8
waynergt <wlopezy2@miumg.edu.gt>
2025-08-10 23:32:11 -0600

    mejorando el diseño de las tarjetas, elegante y traslucidas

--- src/components/SectionCards.tsx
+++ src/components/SectionCards.tsx
@@ -11,13 +11,13 @@ const SectionCards: React.FC = () => (
   <section className="container my-4">
     <div className="row justify-content-center">
       {cards.map((card, idx) => (
-        <div key={idx} className="card-col mb-4 d-flex">
-          <div className="card h-100 w-100 custom-card">
-            <div className="card-body">
+        <div key={idx} className="card-col mb-4 d-flex align-items-stretch">
+          <div className="card h-100 w-100 custom-card d-flex flex-column justify-content-between">
+            <div>
               <h5 className="card-title">{card.title}</h5>
               <p className="card-text">{card.text}</p>
-              <a href="#" className="btn btn-primary">Leer más</a>
             </div>
+            <a href="#" className="btn btn-primary align-self-start mt-2">Leer más</a>
           </div>
         </div>
       ))}
